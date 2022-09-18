1229fa6c4ba60b05c8401f53514aac04548dfc97
Bruno da Silva <feup09@users.noreply.github.com>
2022-09-19 01:12:19 +0200

    User can record sales

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sale;
 use App\Services\SaleService;
 use Illuminate\Http\Request;
 
@@ -23,6 +24,26 @@ class SaleController extends Controller
 
         return response()->json([
             'sellingPrice' => $this->saleService->calculateSellingPrice($request->input('quantity'), $request->input('unitCost'))
+        ]);
+    }
+
+    public function getSales()
+    {
+        return response()->json([
+            'sales' => Sale::all('quantity', 'unit_cost', 'selling_price')
+        ]);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'quantity' => 'required|gt:0',
+            'unitCost' => 'required|gt:0',
+        ]);
+
+        Sale::create([
+            'quantity' => $request->quantity,
+            'unit_cost' => $request->unitCost,
+            'selling_price' => $this->saleService->calculateSellingPrice($request->quantity, $request->unitCost)
         ]);
     }
 }
